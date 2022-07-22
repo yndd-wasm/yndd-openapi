38112137eabae883f48294c4501983a24b854234
Wim Henderickx <wim.henderickx@gmail.com>
2022-07-22 13:04:50 +0200

    added transform response

--- src/v1alpha1/app/mod.rs
+++ src/v1alpha1/app/mod.rs
@@ -3,6 +3,6 @@ pub use self::transform_request::TransformRequest;
 pub use self::transform_request::TransformRequestSpec;
 
 mod transform_response;
-pub use self::transform_request::TransformResponse;
-pub use self::transform_request::TransformResponseSpec;
-pub use self::transform_request::TransformResponseResult;
+pub use self::transform_response::TransformResponse;
+pub use self::transform_response::TransformResponseSpec;
+pub use self::transform_response::TransformResponseResult;
